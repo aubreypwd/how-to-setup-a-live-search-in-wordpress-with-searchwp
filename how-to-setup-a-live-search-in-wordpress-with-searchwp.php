b72b57d05c0a1f19be70a7de8fa59dbdd0d04ca8
Aubrey Portwood <aubreypwd@gmail.com>
2018-11-13 08:46:21 -0700

    Add plugin header.

---
+++ how-to-setup-a-live-search-in-wordpress-with-searchwp.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: How to setup a "live" search in WordPress with SearchWP
+ * Version:     1.0.0
+ * Author:      Aubrey Portwood at WebDevStudios
+ * Author URI:  http://webdevstudios.com
+ * Text Domain: how-to-setup-a-live-search-in-wordpress-with-searchwp
+ * License:     GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @since       1.0.0
+ * @package     WebDevStudios\how-to-setup-a-live-search-in-wordpress-with-searchwp
+ */
